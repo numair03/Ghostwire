813ac38da50d2d2d835c644f4617221f1f1c8d64
Numair <numair.wani@yahoo.com>
2026-08-08 01:00:44 +0530

    Test commit 2

--- client-backend/main.go
+++ client-backend/main.go
@@ -3,46 +3,117 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
+	"os/exec"
+	"runtime"
+	"time"
 
 	"github.com/devlup-labs/Ghostwire/client-backend/wireguard"
 )
 
-func main() {
-	fmt.Println("[GhostWire Client] Initializing node...")
+type RegisterResponse struct {
+	VirtualIP string `json:"virtualIp"`
+}
 
-	// 1. Generate local keys and device instance
+type CheckinResponse struct {
+	Allowlist map[string]struct {
+		PublicKey     []byte `json:"PublicKey"`
+		PublicAddress string `json:"PublicAddress"`
+		GwIp          string `json:"GwIp"`
+	} `json:"allowlist"`
+}
+
+func main() {
+	deviceID := flag.String("id", "device-001", "Unique device ID")
+	port := flag.Int("port", 51820, "WireGuard listen port")
+	fallbackIP := flag.String("ip", "10.0.0.2", "Virtual IP")
+	serverURL := flag.String("server", "http://127.0.0.1:8000", "Coordination server URL")
+	physicalIP := flag.String("endpoint-ip", "127.0.0.1", "Physical LAN IP of this machine")
+	flag.Parse()
+
+	fmt.Printf("[GhostWire Client] Initializing %s...\n", *deviceID)
+
 	node, err := wireguard.NewNode()
 	if err != nil {
 		panic(err)
 	}
 
-	// 2. Start the TUN interface on port 51820.
-	// Passing "utun" lets macOS auto-assign a number (e.g., utun3)
-	realIface, err := node.Start("utun", 51820)
+	realIface, err := node.Start("utun", *port)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to start WireGuard interface: %v", err))
+		panic(err)
 	}
-	fmt.Printf("[GhostWire Client] Virtual interface '%s' is up!\n", realIface)
+	fmt.Printf("[GhostWire Client] Interface '%s' up!\n", realIface)
 
-	// 3. Register with your coordination server API
+	// 1. REGISTRATION
 	registerPayload, _ := json.Marshal(map[string]any{
-		"deviceId":   "device-001",
-		"oAuthToken": "dummy-token",
+		"deviceId":   *deviceID,
+		"oAuthToken": "dummy",
 		"isHealthy":  true,
-		"publicKey":  node.PublicKey.String(), // Safe to send standard Base64 to server
-		"endpoint":   "127.0.0.1:51820",
+		"publicKey":  node.PublicKey.String(),
+		"endpoint":   fmt.Sprintf("%s:%d", *physicalIP, *port),
 	})
 
-	resp, err := http.Post("http://127.0.0.1:8000/api/v1/register", "application/json", bytes.NewBuffer(registerPayload))
+	resp, err := http.Post(*serverURL+"/api/v1/register", "application/json", bytes.NewBuffer(registerPayload))
 	if err != nil {
-		fmt.Printf("[Error] Failed to reach coordination server: %v\n", err)
-	} else {
-		fmt.Printf("[GhostWire Client] Registered with server. Status: %d\n", resp.StatusCode)
-		resp.Body.Close()
+		panic(fmt.Sprintf("Server unreachable at %s: %v", *serverURL, err))
 	}
+	defer resp.Body.Close()
 
-	// Keep client running
-	select {}
+	// FIX: Renamed from regResp to registration to avoid the &reg HTML entity bug
+	var registration RegisterResponse
+	json.NewDecoder(resp.Body).Decode(&registration)
+
+	if registration.VirtualIP == "" {
+		registration.VirtualIP = *fallbackIP
+	}
+	fmt.Printf("[GhostWire] Assigned Virtual IP: %s\n", registration.VirtualIP)
+
+	// 2. OS INTERFACE CONFIGURATION
+	setInterfaceIP(realIface, registration.VirtualIP)
+
+	// 3. HEARTBEAT
+	ticker := time.NewTicker(10 * time.Second)
+	runCheckin(node, *deviceID, registration.VirtualIP, *port, *serverURL)
+	for range ticker.C {
+		runCheckin(node, *deviceID, registration.VirtualIP, *port, *serverURL)
+	}
+}
+
+func runCheckin(node *wireguard.Node, id, ip string, port int, server string) {
+	checkinPayload, _ := json.Marshal(map[string]any{
+		"deviceId": id,
+		"gwIp":     ip,
+		"gwPort":   port,
+	})
+
+	req, _ := http.NewRequest("POST", server+"/api/v1/checkin", bytes.NewBuffer(checkinPayload))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	var checkinResp CheckinResponse
+	json.NewDecoder(resp.Body).Decode(&checkinResp)
+
+	var peers []wireguard.PeerConfig
+	for _, entry := range checkinResp.Allowlist {
+		peers = append(peers, wireguard.PeerConfig{
+			PublicKey: string(entry.PublicKey),
+			Endpoint:  entry.PublicAddress,
+			AllowedIP: entry.GwIp + "/32",
+		})
+	}
+	node.SyncPeers(peers)
+}
+
+func setInterfaceIP(iface, ip string) error {
+	if runtime.GOOS == "darwin" {
+		return exec.Command("ifconfig", iface, ip, ip, "up").Run()
+	}
+	exec.Command("ip", "addr", "add", ip+"/32", "dev", iface).Run()
+	return exec.Command("ip", "link", "set", "dev", iface, "up").Run()
 }
